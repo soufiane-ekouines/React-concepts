18baa94c17c3435ce0dd2cc11ada345ed49b25bc
soufiane-ekouines <soufianeekouines@gmail.com>
2022-10-30 22:15:11 +0100

    hooks => useref

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
-import React from "react"
+import React,{ useRef } from "react";
 
 export const Cart = (props) =>{
+    
+    //to get or change (styles...) the object
+    const divRef = useRef();
+
+    const  changeStyles = () =>{
+        console.log(divRef.current.style.color = "red");
+    }
     return(
-    <div>
+    <div ref={ divRef } onClick={changeStyles}>
         I'm {props.name}
     </div>
     )
