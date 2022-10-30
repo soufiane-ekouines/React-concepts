dc22151e41a3a3d70d724016f2ae680145bb5fef
soufiane-ekouines <soufianeekouines@gmail.com>
2022-10-30 22:01:47 +0100

    Hooks : -useState  -useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import { Box } from "./components/Box";
 import { Cart } from "./components/Card";
 
 function App() {
+
+  //usestate to set name 
+  const [name,setName] = useState('hello boob');
+
   const onBoxClicked = () =>{
     console.log('hello from app')
   }
+
+  // start whene the app start
+  useEffect( () => {
+
+    setInterval(() => {
+      setName('Bob'+Math.random());
+    },1000)
+    // with jaust return is to clean up 
+  },[]);
+
   return (
     <div className="App">
       <Cart name="soufiane"/>
       
-      <Box name={'ekouines'} onClicked={onBoxClicked}/>
+      <Box name={name} onClicked={onBoxClicked}/>
     </div>
   );
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React,{useEffect} from "react";
 
 export const Box = ({name,onClicked}) =>{
+
+    // start whene the components created || with name (start every name change value)
+    useEffect(() => {
+        console.log('we are here');
+    }, [name])
+
     const printHello = () =>{
         console.log('hello from box')
     }
+
     return(
         <div onClick={onClicked}>
             hi props i'm {name}
